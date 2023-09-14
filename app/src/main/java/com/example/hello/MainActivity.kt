3839952ea6bc8b0cdbeaa9a2e89c3c4305e97eac
dell <rahulv080353@gmail.com>
2023-09-14 12:26:51 +0530

    final

---
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var e: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
